c3b67429c77d0012650539bbc01336ab5b3a6030
SSen <i@ssen.name>
2018-01-03 15:29:54 +0900

    Add source-map-support to nodemon.

--- scripts/web.server.dev.start.js
+++ scripts/web.server.dev.start.js
@@ -1,11 +1,16 @@
 const nodemon = require('nodemon');
 const path = require('path');
+const fs = require('fs');
 
 module.exports = (options) => () => {
+  const sourceMapSupportRegister = fs.existsSync(`${options.CWD}/node_modules/source-map-support`)
+    ? `${options.CWD}/node_modules/source-map-support/register`
+    : `${options.MODULE_HOME}/node_modules/source-map-support/register`;
+  
   nodemon({
     watch: [
       path.join(options.CWD, 'dist-dev', 'server/'),
     ],
-    exec: 'node ' + path.join(options.CWD, 'dist-dev', 'server'),
+    exec: 'node -r ' + sourceMapSupportRegister + ' ' + path.join(options.CWD, 'dist-dev', 'server'),
   });
 };
